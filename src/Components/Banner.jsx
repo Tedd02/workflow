634ef60a8971ec6f6bea526fc649bea45eecda57
tedd02 <dtted02@gmail.com>
2025-11-09 22:50:37 +0100

    First commit

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png'
 
 const Banner = () => {
   return (
-    <div class='text-black textalign-right p-8 border-b flex flex-row justify-end-safe items-center'>
+    <div class='text-black textalign-right p-8 border-b border-solid flex flex-row justify-end-safe items-center'>
         <img src={logo} alt="La maison jungle logo" class="h-[45px] w-[45px]"/>
         <h1 class= "p-8">La maison jungle</h1>
     </div>
--- src/Components/ShoppingList.jsx
+++ src/Components/ShoppingList.jsx
@@ -18,9 +18,12 @@ function ShoppingList() {
 				<li key={cat}>{cat}</li>
 			))}
 		</ul>
-		<ul>
+		<ul class='list-none p-8 flex flex-wrap max-w-[800px]'>
 			{plantList.map((plant) => (
-				<li key={plant.id}>{plant.isBestSale ? <span>{plant.name} 🔥</span> : null}</li>
+				<li key={plant.id} class="border-2 border-solid rounded-xl m-2.5 h-[150px] w-[150px] flex flex-col justify-center items-center relative">
+                    {plant.name}
+					{plant.isSpecialOffer && <div class='absolute right-0 top-2.5 py-1 px-2.5 font-medium text-white bg-[#31b572]'>Soldes</div>}
+                </li>
 			))}
 		</ul>
     </div>
